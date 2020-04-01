1b13edcd865c72d66d475fce8fab21b14dbee2d2
Alejandro Goicochea <panflete@Alejandros-MacBook-Pro.local>
2020-04-01 14:13:16 -0500

    Added Log4j with edited pom.xml file.

--- hand_wash/src/main/java/cs_utec/cs2901/App.java
+++ hand_wash/src/main/java/cs_utec/cs2901/App.java
@@ -1,16 +1,21 @@
 package cs_utec.cs2901;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static final Logger logger = Logger.getLogger(App.class);
     public static void main( String[] args )
     {
+        BasicConfigurator.configure();
         System.out.println( "Hello World!" );
     	for(int i = 0; i < 100; i++){
-	    System.out.println("Me debo lavar las manos!");
+	    logger.info("Me debo lavar las manos!!");
         }
     }
 }
